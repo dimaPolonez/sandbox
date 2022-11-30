40bb29267d0dc010766797929f8ed48505cc6e90
dimaPolonez <dima-barxat@yandex.ru>
2022-11-30 09:10:11 +0300

    add: 1 kata CodeWars

---
+++ CW DigitsExplosion.js
@@ -0,0 +1,33 @@
+/* Description:
+
+Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+
+Examples
+explode("312")
+should return :
+
+"333122"
+explode("102269")
+should return :
+
+"12222666666999999999"
+ */
+
+
+/* Solutions: */
+
+function explode(s) {
+  
+    let arrStr = s.split('');
+  
+    let arrResult = [];
+  
+  
+    arrStr.forEach((value) => {
+        for (let i=0; i < Number(value); i++) {
+          arrResult.push(value);
+        }
+    })
+  
+    return arrResult.join('');
+}
